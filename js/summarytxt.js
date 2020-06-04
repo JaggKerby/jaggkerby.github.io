bc3e774296453b01c3b66ae4d9e7b37e2548569a
JaggKerby <ilyakerby@gmail.com>
2020-06-04 14:25:42 +0300

    fix modal
    fix modal add txt

--- js/summarytxt.js
+++ js/summarytxt.js
@@ -1,8 +1,25 @@
-const data = {
-    message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc'
+const msgOne = {
+  message: 'But I&nbsp;must explain to&nbsp;you how all this mistaken idea of&nbsp;denouncing pleasure and praising pain was born and&nbsp;I will give you a&nbsp;complete account of&nbsp;the system, and expound the actual teachings of&nbsp;the great explorer of&nbsp;the truth, the master-builder of&nbsp;human happiness. No&nbsp;one rejects, dislikes, or&nbsp;avoids pleasure itself, because it&nbsp;is&nbsp;pleasure, but because those who do&nbsp;not know how to&nbsp;pursue pleasure rationally encounter consequences that are extremely painful. Nor again is&nbsp;there anyone who loves or&nbsp;pursues or&nbsp;desires to&nbsp;obtain pain of&nbsp;itself, because it&nbsp;is&nbsp;pain, but because occasionally'
+}
+const msgTwo = {
+  message: 'The European languages are members of&nbsp;the same family. Their separate existence is&nbsp;a&nbsp;myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in&nbsp;their grammar, their pronunciation and their most common words. Everyone realizes why a&nbsp;new common language would be&nbsp;desirable: one could refuse to&nbsp;pay expensive translators. To&nbsp;achieve this, it&nbsp;would be&nbsp;necessary to&nbsp;have uniform grammar, pronunciation and more common words. If&nbsp;several languages coalesce, the grammar of&nbsp;the resulting language is&nbsp;more simple and regular than that of&nbsp;the individual languages. The new common language will be&nbsp;more'
+}
+const msgThree = {
+  message: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in&nbsp;Bookmarksgrove right at&nbsp;the coast of&nbsp;the Semantics, a&nbsp;large language ocean. A&nbsp;small river named Duden flows by&nbsp;their place and supplies it&nbsp;with the necessary regelialia. It&nbsp;is&nbsp;a&nbsp;paradisematic country, in&nbsp;which roasted parts of&nbsp;sentences fly into your mouth. Even the all-powerful Pointing has no&nbsp;control about the blind texts it&nbsp;is&nbsp;an&nbsp;almost unorthographic life One day however a&nbsp;small line of&nbsp;blind text by&nbsp;the name of&nbsp;Lorem Ipsum decided&nbsp;to'
+}
+const msgFour = {
+  message: 'A&nbsp;wonderful serenity has taken possession of&nbsp;my&nbsp;entire soul, like these sweet mornings of&nbsp;spring which&nbsp;I enjoy with my&nbsp;whole heart. I&nbsp;am alone, and feel the charm of&nbsp;existence in&nbsp;this spot, which was created for the bliss of&nbsp;souls like mine. I&nbsp;am so&nbsp;happy, my&nbsp;dear friend, so&nbsp;absorbed in&nbsp;the exquisite sense of&nbsp;mere tranquil existence, that&nbsp;I neglect my&nbsp;talents. I&nbsp;should be&nbsp;incapable of&nbsp;drawing a&nbsp;single stroke at&nbsp;the present moment; and yet&nbsp;I feel that&nbsp;I never was a&nbsp;greater artist than now. When, while the lovely valley teems with'
+}
+const msgFive = {
+  message: 'One morning, when Gregor Samsa woke from troubled dreams, he&nbsp;found himself transformed in&nbsp;his bed into a&nbsp;horrible vermin. He&nbsp;lay on&nbsp;his armour-like back, and if&nbsp;he&nbsp;lifted his head a&nbsp;little he&nbsp;could see his brown belly, slightly domed and divided by&nbsp;arches into stiff sections. The bedding was hardly able to&nbsp;cover it&nbsp;and seemed ready to&nbsp;slide off any moment. His many legs, pitifully thin compared with the size of&nbsp;the rest of&nbsp;him, waved about helplessly as&nbsp;he&nbsp;looked. &laquo;What&rsquo;s happened to&nbsp;me?&raquo; he&nbsp;thought. It&nbsp;wasn&rsquo;t a&nbsp;dream. His room, a&nbsp;proper human'
+}
+const msgSix = {
+  message: 'The quick, brown fox jumps over a&nbsp;lazy dog. DJs flock by&nbsp;when MTV ax&nbsp;quiz prog. Junk MTV quiz graced by&nbsp;fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my&nbsp;junk&nbsp;TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my&nbsp;big quiz. Quick, Baz, get my&nbsp;woven flax jodhpurs! &laquo;Now fax quiz Jack!&raquo; my&nbsp;brave'
+}
 
-  }
-  
-  $('#msg, #msg2, #msg3, #msg4, #msg5, #msg6').html(data.message)
-  
-  console.log(data)
+$('#msg').html(msgOne.message)
+$('#msg2').html(msgTwo.message)
+$('#msg3').html(msgThree.message)
+$('#msg4').html(msgFour.message)
+$('#msg5').html(msgFive.message)
+$('#msg6').html(msgSix.message)
